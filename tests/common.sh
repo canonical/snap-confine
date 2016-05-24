b8125b14dbe6fedf8900404909e59244db37d8b7
Michael Vogt <mvo@ubuntu.com>
2016-05-24 09:09:04 +0200

    Implment setting app specific environment vars
    This implements setting environment variables that got defined via
    the snapd daemon. This is useful to avoid having to manually write
    wrappers in snapcraft. Instead the environment is part of meta/snap.yaml
    and will get generated automatically on install by snapd.

--- tests/common.sh
+++ tests/common.sh
@@ -6,7 +6,11 @@ TMP="$(mktemp -d)"
 trap "rm -rf $TMP" EXIT
 
 export SNAPPY_LAUNCHER_SECCOMP_PROFILE_DIR="$TMP"
+export SNAPPY_LAUNCHER_ENVIRONMENT_DIR="$TMP/environment"
+mkdir "$SNAPPY_LAUNCHER_ENVIRONMENT_DIR"
+
 export SNAPPY_LAUNCHER_INSIDE_TESTS="1"
+
 
 FAIL() {
     printf ": FAIL\n"
