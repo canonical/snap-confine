6585e0dd1a6645c70c7c6d8c9d5041198b864d5e
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-05-27 20:12:00 +0200

    Change how core snap mounts are created
    This patch fundamentally changes how core snap bind mounts are created
    when running on a classic distribution. To align with how an all-snap
    distribution looks like, the root filesystem is now a read only core
    snap. From that snap a number of bind mounts reach to the classic system
    (for user and system writable data and a few other things).
    
    Lastly since the runtime environment is now identical in all places, we
    should reset PATH to always aligned with the core snap.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- src/main.c
+++ src/main.c
@@ -26,6 +26,7 @@
 #include <sys/mount.h>
 #include <sys/apparmor.h>
 #include <sys/stat.h>
+#include <sys/syscall.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
@@ -342,40 +343,70 @@ void setup_private_pts()
 	}
 }
 
-void setup_snappy_os_mounts()
+void setup_core_snap_mounts()
 {
 	debug("%s", __func__);
-
-	// we mount some whitelisted directories
+	char rootfs_dir[MAX_BUF] = { 0 };
+	// Create a temporary directory that will become the root directory of this
+	// process later on. The directory will be used as a mount point for the
+	// core snap.
 	//
-	// Note that we do not mount "/etc/" from snappy. We could do that,
-	// but if we do we need to ensure that data like /etc/{hostname,hosts,
-	// passwd,groups} is in sync between the two systems (probably via
-	// selected bind mounts of those files).
-	const char *mounts[] =
-	    { "/bin", "/sbin", "/lib", "/lib32", "/libx32", "/lib64", "/usr" };
-	for (int i = 0; i < sizeof(mounts) / sizeof(char *); i++) {
-		// we mount the OS snap /bin over the real /bin in this NS
-		const char *dst = mounts[i];
-
-		char buf[512];
-		must_snprintf(buf, sizeof(buf), "/snap/ubuntu-core/current/%s",
-			      dst);
-		const char *src = buf;
-
-		// some system do not have e.g. /lib64
-		struct stat sbuf;
-		if (stat(dst, &sbuf) != 0 || stat(src, &sbuf) != 0) {
-			if (errno == ENOENT)
-				continue;
-			else
-				die("could not stat mount point");
+	// XXX: This directory is never cleaned up today.
+	must_snprintf(rootfs_dir, sizeof(rootfs_dir), "/tmp/snap.rootfs_XXXXXX");
+	if (mkdtemp(rootfs_dir) == NULL) {
+		die("cannot create temporary directory for the root file system");
+	}
+	// Bind mount the OS snap into the rootfs directory.
+	const char *core_snap_dir = "/snap/ubuntu-core/current";
+	debug("bind mounting core snap: %s -> %s", core_snap_dir, rootfs_dir);
+	if (mount(core_snap_dir, rootfs_dir, NULL, MS_BIND, NULL) != 0) {
+		die("cannot bind mount core snap: %s to %s", core_snap_dir, rootfs_dir);
+	}
+	// Bind mount certain directories from the rootfs directory (with the core
+	// snap) to various places on the host OS. Each directory is justified with
+	// a short comment below.
+	const char *source_mounts[] = {
+		"/dev", // because it contains devices on host OS
+		"/etc", // because that's where /etc/resolv.conf lives, perhaps a bad idea
+		"/home", // to support /home/*/snap and home interface
+		"/proc", // fundamental filesystem
+		"/snap",  // to get access to all the snaps
+		"/sys", // fundamental filesystem
+		"/tmp",  // to get writable tmp
+		"/var/lib/snapd" , // to get access to snap state
+		"/var/snap" , // to get access to global snap data
+		"/var/tmp" , // to get access to the other temporary directory
+	};
+	for (int i = 0; i < sizeof(source_mounts) / sizeof *source_mounts; i++) {
+		const char *src = source_mounts[i];
+		char dst[512];
+		must_snprintf(dst, sizeof dst, "%s%s", rootfs_dir, source_mounts[i]);
+		debug("bind mounting %s to %s", src, dst);
+		// NOTE: MS_REC so that we can see anything that may be mounted under
+		// any of the directories already. This is crucial for /snap, for
+		// example.
+		//
+		// NOTE: MS_SLAVE so that the started process cannot maliciously mount
+		// anything into those places and affect the system on the outside.
+		if (mount(src, dst, NULL, MS_BIND | MS_REC | MS_SLAVE, NULL) != 0) {
+			die("cannot bind mount %s to %s", src, dst);
 		}
-
-		debug("mounting %s -> %s\n", src, dst);
-		if (mount(src, dst, NULL, MS_BIND, NULL) != 0) {
-			die("unable to bind %s to %s", src, dst);
-		}
+	}
+	// Chroot into the new root filesystem so that / is the core snap.  Why are
+	// we using something as esoteric as pivot_root? Because this makes apparmor
+	// handling easy. Using a normal chroot makes all apparmor rules conditional.
+	// We are either running on an all-snap system where this would-be chroot
+	// didn't happen and all the rules see / as the root file system _OR_
+	// we are running on top of a classic distribution and this chroot has now
+	// moved all paths to /tmp/snap.rootfs_*. Because we are using unshare with
+	// CLONE_NEWNS we can essentially use pivot_root just like chroot but this
+	// makes apparmor unaware of the old root so everything works okay.
+	debug("chrooting into %s", rootfs_dir);
+	if (chdir(rootfs_dir) == -1) {
+		die("cannot change working directory to %s", rootfs_dir);
+	}
+	if (syscall(SYS_pivot_root, ".", rootfs_dir) == -1) {
+		die("cannot pivot_root to the new root filesystem");
 	}
 }
 
@@ -514,8 +545,14 @@ int main(int argc, char **argv)
 
 		// do the mounting if run on a non-native snappy system
 		if (is_running_on_classic_distribution()) {
-			setup_snappy_os_mounts();
+			setup_core_snap_mounts();
+			// Reset path as we cannot rely on the path from the host OS to
+			// make sense. The classic distribution may use any PATH that makes
+			// sense but we cannot assume it makes sense for the core snap
+			// layout.
+			setenv("PATH", "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin", 1);
 		}
+
 		// set up private mounts
 		setup_private_mount(appname);
 
